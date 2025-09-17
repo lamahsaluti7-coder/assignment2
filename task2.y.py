c3a448017da13a26895b6f16f10d743e91c90b13
lamahsaluti7-coder <lamahsaluti7@gmail.com>
2025-09-16 21:40:59 -0700

    Initial commit

---
+++ task2.y.py
@@ -0,0 +1,16 @@
+# Constants
+PI = 3.14159
+GRAVITY = 9.81  # m/s^2
+
+# Variables
+radius = 5
+height = 10
+
+# Calculating the volume of a cylinder
+volume = PI * (radius ** 2) * height
+
+# Printing the results
+print("Volume of the cylinder:")
+print("Radius:", radius)
+print("Height:", height)
+print("Calculated Volume:", volume)
